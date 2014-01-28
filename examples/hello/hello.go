1fc17f4a9219d559ac3dbc481f79662e088b8d16
zeekay <zk@monoid.io>
2014-01-28 02:04:34 -0800

    Fix whitespace.

--- examples/hello/hello.go
+++ examples/hello/hello.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
     zip.Get("/", func(req zip.Req, res zip.Res) {
-	    res.Header.Set("Content-Type", "text/plain")
+        res.Header.Set("Content-Type", "text/plain")
         res.WriteString("hello world!")
     })
     zip.Listen(":1337")
