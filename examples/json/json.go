ef3777b549d808de20a94abe26e31fbbca587870
zeekay <zk@monoid.io>
2014-01-28 03:52:49 -0800

    Update examples.

--- examples/json/json.go
+++ examples/json/json.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+    "log"
     "zeekay.io/zip"
 )
 
-type json struct {
+type helloJson struct {
     Hello string `json:"hello"`
 }
 
 func main() {
+    // You can easily return JSON data with res.JSON
     zip.Get("/json", func(req zip.Req, res zip.Res) {
-        res.JSON(&json{Hello: "world!"})
+        res.JSON(&helloJson{Hello: "world!"})
+    })
+
+    // ...and decode JSON sent in a POST body with req.JSON(),
+    // for example:
+    // $ curl 127.0.0.1:1337/json -d '{"hello": "world"}'
+    zip.Post("/json", func(req zip.Req, res zip.Res) {
+        data := helloJson{}
+        req.JSON(&data)
+
+        // ...or so something more interesting with it.
+        log.Println(data)
     })
     zip.Listen(":1337")
 }
--- request.go
+++ request.go
@@ -10,7 +10,7 @@ type Req struct {
     *http.Request
 }
 
-func (r *Req) JSON(s *interface{}) (err error) {
+func (r *Req) JSON(s interface{}) (err error) {
     decoder := json.NewDecoder(r.Body)
     return decoder.Decode(&s)
 }
